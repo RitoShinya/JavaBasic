8482d437f468cf35b1f0073ac775134164efb560
RitoShinya <RitoShinya@users.noreply.github.com>
2019-02-08 12:33:15 +0900

    1-7

--- Practice/src/practice01/PTra01_02.java
+++ Practice/src/practice01/PTra01_02.java
@@ -9,10 +9,10 @@ package practice01;
 public class PTra01_02 {
 	public static void main(String[] args) {
 
-		// ★ 12～14行目をコメントにしてください。
-		ここをコメントにしてください
-		ここをコメントにしてください
-		ここをコメントにしてください
+		System.out.println("Hello,world");
+		System.out.println("こんにちは");
+		System.out.println("よろしくお願いします");
+
 
 		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください
 
