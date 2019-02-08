82f5009f2c32c55b0331c91c950a60ac104d7d67
RitoShinya <RitoShinya@users.noreply.github.com>
2019-02-08 14:04:45 +0900

    2-10

--- Practice/src/practice02/PTra02_09.java
+++ Practice/src/practice02/PTra02_09.java
@@ -12,16 +12,18 @@ public class PTra02_09 {
 		int height = 23;	// 高さ
 
 		// ★ 変数quadrangleに、四角形の面積を代入してください
-
+         int quadrangle;
+         quadrangle=9*23;
 
 		// ★ 変数quadrangleを出力してください
-
+        System.out.println(quadrangle);
 
 		// ★ 変数triangleに、三角形の面積を代入してください
-
+        int triangle;
+        triangle=quadrangle/2;
 
 		// ★ 変数triangleを出力してください
-
+        System.out.println(triangle);
 
 	}
 }
