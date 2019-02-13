8189f63060a56dfc70072c10ae67a8451f8b2f29
RitoShinya <RitoShinya@users.noreply.github.com>
2019-02-13 10:46:07 +0900

    6-7

--- Practice/src/practice06/PTra06_07.java
+++ Practice/src/practice06/PTra06_07.java
@@ -24,6 +24,26 @@ public class PTra06_07 {
 		 * ■■□□□
 		 * ■□□□□
 		 */
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("■");
+			}
+			    for (int k = 0; k < 5-i ; k++) {
+		            System.out.print("□");
+			}
+			System.out.println();
+		}
+		for (int l = 0; l < 5; l++) {
+			for (int m = 0; m < 5-l; m++) {
+				System.out.print("■");
+			}
+			    for (int n = 0; n < l ; n++) {
+		            System.out.print("□");
+			}
+
+		System.out.println();
+		}
+
 
 	}
 }
